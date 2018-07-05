fc6b9469222a30336de533623b2d7c9319b7754e
Irina Brovar <iris-arcus@yandex.ru>
2018-07-06 00:38:20 +0300

    Add functions, fix local resources

--- crash-course/use-local-resources.sh
+++ crash-course/use-local-resources.sh
@@ -6,4 +6,5 @@ for file in $HTML_FILES; do
   sed -e s/src=\\\"https:\\/\\/cdnjs\\.cloudflare\\.com\\/[^\\\"]*\\/require\\.min\\.js\\\"/src=\\\"files\\/require.min.js\\\"/ -i "$file"
   sed -e s/src=\\\"https:\\/\\/cdnjs\\.cloudflare\\.com\\/[^\\\"]*\\/jquery\\.min\\.js\\\"/src=\\\"files\\/jquery.min.js\\\"/ -i "$file"
   sed -e s/src=\\\"https:\\/\\/cdn\\.mathjax\\.org\\/[^\\\"]*\\/MathJax\\.js/src=\\\"files\\/MathJax.js/ -i "$file"
+  sed -e s/src=\\\"https:\\/\\/cdnjs\\.cloudflare\\.com\\/[^\\\"]*\\/MathJax\\.js/src=\\\"files\\/MathJax.js/ -i "$file"
 done
